0738dc2381a4106937ccc6809e3d232c24d0d63c
victor_perezvaldez@hotmail.com <victor_perezvaldez@hotmail.com>
2022-03-23 14:00:39 -0600

    X agregada en menu hamburguesa

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const menu = document.querySelector('.menu');
-const navbar = document.querySelector('.navbar')
+const navbar = document.querySelector('.navbar');
+const divIcon = document.querySelector('.menu-responsive')
 menu.addEventListener('click',show);
 
 function show(){
     if(navbar.className === "navbar"){
         navbar.className += " show";
+        divIcon.classList.remove('menu-responsive');
+        divIcon.classList.add('menu-responsive-active')
+        
     }else{
         navbar.className = "navbar"
+        divIcon.classList.remove('menu-responsive-active');
+        divIcon.classList.add('menu-responsive')
     }
 }
